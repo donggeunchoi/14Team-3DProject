53716ba9e37079ea0c9cfb97a77d3f0afd77ea14
donggeunchoi <cdg01063149045@gmail.com>
2025-05-26 15:58:19 +0900

    feat : 게임 진행 UI 스크립트 완성

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static UIManager Instance { get; private set; }
+    
+    [Header("Combo & Score")]
+    public TextMeshProUGUI comboText;
+    public TextMeshProUGUI scoreText;
+
+    [Header("Time")] 
+    public TimeUI timeUI;
+
+    private int score = 0;
+    private int combo = 0;
+    
     void Start()
     {
-        
+        UpdateScore(0);
+        UpdateCombo(0);
+        timeUI.ResetTime();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int amount)
     {
-        
+        score += amount;
+        UpdateScore(score);
     }
+
+    public void IncreaseCombo()
+    {
+        combo++;
+        UpdateCombo(combo);
+    }
+
+    public void ResetCombo()
+    {
+        combo = 0;
+        UpdateCombo(combo);
+    }
+
+    private void UpdateScore(int newScore)
+    {
+        scoreText.text = newScore.ToString();
+    }
+
+    private void UpdateCombo(int newCombo)
+    {
+        comboText.text = newCombo.ToString();
+    }
+
+    
 }
