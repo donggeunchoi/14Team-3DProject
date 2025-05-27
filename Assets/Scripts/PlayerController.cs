95fa629aa91fe87e05e0ec2e377d861af24d5a15
jjmm-del <jimin8768@naver.com>
2025-05-27 12:05:06 +0900

    add: roll
    roll 기능 구현 불필요하면 주석처리 or 삭제

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    [Header("MovementSetting")]
+    [Header("Movement Setting")]
     public float moveSpeed; //플레이어 기본 전진 속도 앞으로
     public float laneChangeSpeed; //좌우 이동 속도
     public float jumpForce; // 점프력
     public float laneDistance; //레인 간 거리
-    private int laneCount = 3;
-    private int currentLaneIndex = 0;  //-1,0,1 *landDistance해서 배치
     public LayerMask groundLayerMask;
+    
+    private int _laneCount = 3;
+    private int _currentLaneIndex = 0;  //-1,0,1 *landDistance해서 배치
 
-    
-    
+    [Header("Roll Setting")]
+    public float rollDuration; // 구르기 지속시간
+    public float slideColliderHeight; // 굴렀을 때 콜라이더 높이
+    public float slideColliderCenterY; // 굴렀을때 콜라이더 y좌표
+
+    private bool isRolling = false;
+    private float _originalColldierHeight;
+    private Vector3 _originalColliderCenter;
+    private CapsuleCollider _capsuleCollider;
     [HideInInspector] public bool canLook = true;
 
     private Rigidbody _rigidbody;
@@ -23,6 +31,10 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _capsuleCollider = GetComponent<CapsuleCollider>();
+        _originalColldierHeight = _capsuleCollider.height;
+        _originalColliderCenter = _capsuleCollider.center;
+        
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -42,15 +54,15 @@ public class PlayerController : MonoBehaviour
 
     private void MoveForward()
     {
-        Vector3 forwardMovement = Vector3.forward * moveSpeed * Time.deltaTime;
+        Vector3 forwardMovement = Vector3.forward * (moveSpeed * Time.deltaTime);
         _rigidbody.MovePosition(_rigidbody.position + forwardMovement);
     }
 
     private void UpdateLanePosition()
     {
-        float targetX = (currentLaneIndex) * laneDistance;
+        float targetX = (_currentLaneIndex) * laneDistance;
         Vector3 targetPosition = new Vector3(targetX, _rigidbody.position.y, _rigidbody.position.z)
-            + Vector3.forward * moveSpeed*Time.deltaTime;
+            + Vector3.forward * (moveSpeed * Time.deltaTime);
         
         Vector3 newPosition = Vector3.Lerp(_rigidbody.position, targetPosition,laneChangeSpeed*Time.deltaTime);
         _rigidbody.MovePosition(newPosition);
@@ -68,20 +80,27 @@ public class PlayerController : MonoBehaviour
 
     public void OnMoveRightInput(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Started && currentLaneIndex < 1)
+        if (context.phase == InputActionPhase.Started && _currentLaneIndex < 1)
         {
-            currentLaneIndex++;
+            _currentLaneIndex++;
         }
     }
 
     public void OnMoveLeftInput(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Started && currentLaneIndex > -1)
+        if (context.phase == InputActionPhase.Started && _currentLaneIndex > -1)
         {
-            currentLaneIndex--;
+            _currentLaneIndex--;
         }
     }
-
+    
+    public void onRollInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded() && isRolling == false)
+        {
+            Roll();
+        }
+    }
    
     bool IsGrounded()
     {
@@ -101,5 +120,18 @@ public class PlayerController : MonoBehaviour
         }
 
         return false;
+    }
+
+    private IEnumerator Roll()
+    {
+        isRolling = true;
+        
+        _capsuleCollider.height  = slideColliderHeight;
+        _capsuleCollider.center  = new Vector3(_originalColliderCenter.x, slideColliderCenterY, _originalColliderCenter.z);
+        yield return new WaitForSeconds(rollDuration);
+        
+        _capsuleCollider.height = _originalColldierHeight;
+        _capsuleCollider.center = _originalColliderCenter;
+        isRolling = false;
     }
 }
