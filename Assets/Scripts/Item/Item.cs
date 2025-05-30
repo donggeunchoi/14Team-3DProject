ef6857ac384d885add1a7efa3060593f9ef6d87f
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-05-30 14:52:45 +0900

    복구: develop에서 Prefabs/Item과 Scripts/Item 복사

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    public float itemMoveSpeed = 25f; // 아이템 이동 속도
+
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = Vector3.back * itemMoveSpeed; // 장애물 이동속도
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // 충돌한 플레이어의 PlayerCondition 컴포넌트를 가져옴
+            PlayerCondition player = other.GetComponent<PlayerCondition>();
+            // 무적 10초
+            player.ActivateInvincibility(10f);
+
+            if (ItemSpawner.Instance != null)
+            {
+                ItemSpawner.Instance.ReturnItemToPool(gameObject); // 이 아이템 객체를 풀로 반환
+            }
+        }
+    }
+}
